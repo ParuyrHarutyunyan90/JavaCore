ab1279b2dae7e67eff7ec21fdd6cb174a86ebf1c
ParuyrHarutyunyan90 <pash111190pash@mail.ru>
2022-05-10 22:53:54 +0400

    HomeworkFigure2

---
+++ src/homework/FigurePainter1_3.java
@@ -0,0 +1,19 @@
+package homework;
+
+public class FigurePainter1_3 {
+    public static void main(String[] args) {
+
+        for (int i = 0; i < 5; i++) {
+
+            for (int j = 5; j > i; j--) {
+                System.out.print("* ");
+
+            }
+            System.out.println();
+
+
+        }
+
+
+    }
+}
---
+++ src/homework/FigurePainter2_2.java
@@ -0,0 +1,34 @@
+package homework;
+
+public class FigurePainter2_2 {
+    public static void main(String[] args) {
+
+
+        for (int i = 0; i < 4; i++) {
+
+            for (int j = 3; j > i; j--) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j <= i; j++) {
+                System.out.print(" *");
+            }
+            System.out.println();
+        }
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j <= i; j++) {
+                System.out.print(" ");
+
+            }
+            for (int j = 3; j > i; j--) {
+                System.out.print(" *");
+
+            }
+            System.out.println();
+        }
+
+
+    }
+
+
+}
+
